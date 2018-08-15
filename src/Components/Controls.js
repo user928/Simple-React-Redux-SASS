df236b67cf4518f79f690d9effa0c1201c1162ad
Jovan Milovanovic <jmilovanovic@gogoair.com>
2018-08-15 09:47:39 +0200

    refactoring tests

--- src/Components/Controls.js
+++ src/Components/Controls.js
@@ -4,7 +4,7 @@ import store from '../Redux/store';
 import PropTypes from 'prop-types';
 import { decrementNum, incrementNum, resetCounter } from '../Redux/actionCreators/counterActions';
 
-export class _Controlls extends React.Component {
+export class _Controls extends React.Component {
 	static propTypes = {
 		increment: PropTypes.func,
 		decrement: PropTypes.func,
@@ -22,7 +22,7 @@ export class _Controlls extends React.Component {
 				</div>
 
 				<div className="reset" onClick={this.props.resetCount}>
-					<span>Reset this</span>
+					<span>Reset</span>
 				</div>
 
 				<div className="decrement" onClick={this.props.decrement}>
@@ -39,16 +39,16 @@ const mapDispatchToProps = dispatch => ({
 	resetCount: () => dispatch(resetCounter()),
 });
 
-const Controlls = connect(
+const Controls = connect(
 	null,
 	mapDispatchToProps
-)(_Controlls);
+)(_Controls);
 
 export default class extends React.Component {
 	render() {
 		return (
 			<Provider store={store}>
-				<Controlls {...this.props} />
+				<Controls {...this.props} />
 			</Provider>
 		);
 	}
--- src/__test__/Controlls.test.js
+++ src/__test__/Controlls.test.js
@@ -1,32 +1,26 @@
-import Controls from '../Components/Controls.js';
-
+import { _Controls as Controls } from '../Components/Controls';
 
 describe('Controls component', () => {
-
 	it('renders correctly', () => {
-		const wrapper = render(
-			<Controls/>
-		);
+		const wrapper = render(<Controls />);
 
 		expect(wrapper).toMatchSnapshot();
 	});
 
 	it('find reset txt', () => {
-		const wrapper = mount(
-			<Controls />
-		);
-		const resetTxt = wrapper.find('.reset').childAt(0).text();
+		const wrapper = mount(<Controls />);
+		const resetTxt = wrapper
+			.find('.reset')
+			.childAt(0)
+			.text();
 		expect(resetTxt).toBe('Reset');
 	});
 
 	it('click on increment once', () => {
 		const spy = sinon.spy();
-		const wrapper = mount(
-			<Controls increment={spy} />
-		);
+		const wrapper = mount(<Controls increment={spy} />);
 
 		wrapper.find('.increment').simulate('click');
 		expect(spy.calledOnce).toBe(true);
 	});
-
 });
